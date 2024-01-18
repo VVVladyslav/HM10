f074eb3c5ee2b63767610734517986f9ece12cf1
VVVladyslav <vladoszhar@gmail.com>
2024-01-18 23:21:50 +0200

    remake

--- src/main/java/org/example/HttpStatusImageDownloader.java
+++ src/main/java/org/example/HttpStatusImageDownloader.java
@@ -8,25 +8,25 @@ import java.net.URL;
 
 public class HttpStatusImageDownloader {
     public void downloadStatusImage(int code) throws IOException {
+
         String imageUrl = "https://http.cat/" + code + ".jpg";
         HttpURLConnection connection = (HttpURLConnection) new URL(imageUrl).openConnection();
         connection.setRequestMethod("GET");
-        int responseCode = connection.getResponseCode();
+        HttpStatusChecker httpStatusChecker = new HttpStatusChecker();
+        try {
 
-        if (responseCode == HttpURLConnection.HTTP_OK) {
-            try (InputStream inputStream = connection.getInputStream()) {
-                String fileName = "ImageDownloaded" + code + ".jpg";
-                try (FileOutputStream outputStream = new FileOutputStream(fileName)) {
-                    byte[] buffer = new byte[17 * 60];
-                    int bytesRead;
-                    while ((bytesRead = inputStream.read(buffer)) != -1) {
-                        outputStream.write(buffer, 0, bytesRead);
+                try (InputStream inputStream = connection.getInputStream()) {
+                    String fileName = "ImageDownloaded" + code + ".jpg";
+                    try (FileOutputStream outputStream = new FileOutputStream(fileName)) {
+                        byte[] buffer = new byte[17 * 60];
+                        int bytesRead;
+                        while ((bytesRead = inputStream.read(buffer)) != -1) {
+                            outputStream.write(buffer, 0, bytesRead);
+                        }
                     }
                 }
-                System.out.println("Image downloaded: " + fileName);
-            }
-        } else {
-            throw new RuntimeException("Failed to download for code: " + responseCode);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
         connection.disconnect();
     }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,20 +1,7 @@
 package org.example;
 
-import java.io.IOException;
-
 public class Main {
     public static void main(String[] args){
-
-///////////////////////////////////////////////////////////////////////////////////////////////1
-        HttpStatusChecker httpStatusChecker = new HttpStatusChecker();
-        try {
-            String imageUrl200 = httpStatusChecker.getStatusImage(418);
-            System.out.println("Image for code: =   " + imageUrl200);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-
 ///////////////////////////////////////////////////////////////////////////////////////////////2
         HttpStatusImageDownloader httpStatusImageDownloader = new HttpStatusImageDownloader();
         try {
@@ -22,9 +9,6 @@ public class Main {
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-
-
 
         ///////////////////////////////////////////////////////////////////////////////////////////////3
         HttpImageStatusCli httpImageStatusCli = new HttpImageStatusCli();
