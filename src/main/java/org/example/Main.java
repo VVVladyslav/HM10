4e105ee628ba3482d70b923d90721ecca5bbdb20
VVVladyslav <138609115+VVVladyslav@users.noreply.github.com>
2024-01-19 21:55:04 +0200

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,18 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args){
-///////////////////////////////////////////////////////////////////////////////////////////////2
-
-//        HttpStatusImageDownloader httpStatusImageDownloader = new HttpStatusImageDownloader();
-//        try {
-//            httpStatusImageDownloader.downloadStatusImage(418);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-        ///////////////////////////////////////////////////////////////////////////////////////////////3
         HttpImageStatusCli httpImageStatusCli = new HttpImageStatusCli();
         httpImageStatusCli.askStatus();
-
-
     }
 }
